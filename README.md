78e23e03fcbca48ac1fbf6b555bb882b562f1f36
geovanysan <geovanny1santiago@gmail.com>
2021-05-08 14:08:56 -0500

    resquest

---
+++ README.md
@@ -0,0 +1,8 @@
+# node-crud
+npm init -y package.json
+
+npm install express framework http
+
+npm i mysql express-myconnection
+
+npm i nodemode --save-dev
--- routes.js
+++ routes.js
@@ -1,67 +1,66 @@
 const express = require('express')
 const routes = express.Router()
 
-routes.get('/select',(req,res)=>{
-	req.getConnection((err,conn)=>{
-		if(err)return res.send(err)
+routes.get('/select', (req, res) => {
+	req.getConnection((err, conn) => {
+		if (err) return res.send(err)
 
-		conn.query('SELECT * FROM productos',(err,rows)=>{
-			if (err)return res.send(err)
-
-			res.json(rows)
-		})
-	})
-})
-routes.get('/user/:ced',(req,res)=>{
-	req.getConnection((err,conn)=>{
-		if(err)return res.send(err)
-
-		conn.query('SELECT * FROM user where ced = ? ',[req.params.id],(err,rows)=>{
-			if (err)return res.send(err)
+		conn.query('SELECT * FROM productos', (err, rows) => {
+			if (err) return res.send(err)
 
 			res.json(rows)
 		})
 	})
 })
-routes.get('/turnos/',(req,res)=>{
-	req.getConnection((err,conn)=>{
-		if(err)return res.send(err)
+routes.get('/:id', (req, res) => {
+	req.getConnection((err, conn) => {
+		if (err) return res.send(err)
 
-		conn.query('SELECT * FROM productos where fecha = ? and hora ? ',[req.body.fecha,req.body.hora],(err,rows)=>{
-			if (err)return res.send(err)
+		conn.query('SELECT * FROM productos where precio_unit = ?  ', [req.params.id], (err, rows) => {
+			if (err) return res.send(err)
 
 			res.json(rows)
 		})
 	})
 })
-routes.post('/',(req,res)=>{
-	req.getConnection((err,conn)=>{
-		if(err)return res.send(err)
+routes.post('/turnos', (req, res) => {
+	req.getConnection((err, conn) => {
+		if (err) return res.send(err)
 
-		conn.query('INSERT INTO productos set ?',[req.body],(err,rows)=>{
-			if (err)return res.send(err)
+		conn.query('SELECT * FROM productos where precio_unit = ? and precio_dist = ?  ', [req.body.precio_unit, req.body.precio_dist], (err, rows) => {
+			if (err) return res.send(err)
+			res.json(rows)
+		})
+	})
+})
+routes.post('/', (req, res) => {
+	req.getConnection((err, conn) => {
+		if (err) return res.send(err)
+
+		conn.query('INSERT INTO productos set ?', [req.body], (err, rows) => {
+			if (err) return res.send(err)
 
 			res.send('agregado')
 		})
 	})
 })
-routes.delete('/:id',(req,res)=>{
-	req.getConnection((err,conn)=>{
-		if(err)return res.send(err)
+routes.delete('/:id', (req, res) => {
+	req.getConnection((err, conn) => {
+		if (err) return res.send(err)
 
-		conn.query('DELETE  FROM productos where id_prod = ?',[req.params.id],(err,rows)=>{
-			if (err)return res.send(err)
+		conn.query('DELETE  FROM productos where id_prod = ?', [req.params.id], (err, rows) => {
+			if (err) return res.send(err)
 
 			res.send('eleiminado')
 		})
 	})
 })
-routes.put('/:id',(req,res)=>{
-	req.getConnection((err,conn)=>{
-		if(err)return res.send(err)
+routes.put('/:id', (req, res) => {
+	req.getConnection((err, conn) => {
+		if (err) return res.send(err)
 
-		conn.query('UPDATE  FROM productos set ? where id_prod = ?',[req.body,req.params.id],(err,rows)=>{
-			if (err)return res.send(err)
+		conn.query('UPDATE  FROM productos set ? where id_prod = ?', [req.body, req.params.id], (err, rows) => {
+			if (err) return res.send(err)
 
 			res.send('libro actualizado')
 		})
